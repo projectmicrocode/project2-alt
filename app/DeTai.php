b3f7fdbad8f3013edec81b9ad6aa3565282c70e3
dunglunkl0508 <dunglunkl0508@gmail.com>
2017-03-23 11:00:15 +0700

    commit 1100

--- app/DeTai.php
+++ app/DeTai.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DeTai extends Model
 {
     protected $table = 'detai';
     protected $guarded = [];
+
+    public static function getDetai() {
+	$value=DB::table('detai')->get();
+	return $value;
 }
+
+}
+
+
